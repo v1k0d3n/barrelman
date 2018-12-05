5960fb37fb25e083cbc48ecf02e30417445d4fa9
Dan Taylor <C-Dan.Taylor@charter.com>
2018-12-05 10:56:07 -0700

    WIP

--- cluster/release_test.go
+++ cluster/release_test.go
@@ -18,279 +18,300 @@ import (
 )
 
 func TestListReleases(t *testing.T) {
-
-	Convey("Release.go", t, func() {
-		s := NewMockSession()
-		Convey("ListReleases()", func() {
-			Convey("Can fail", func() {
-				r := []*hapi_release5.Release{
-					{
-						Name: "something",
-						Info: &release.Info{
-							Status: &release.Status{
-								Code: release.Status_DEPLOYED,
-							},
+	s := NewMockSession()
+	Convey("ListReleases", t, func() {
+		Convey("Can fail", func() {
+			r := []*hapi_release5.Release{
+				{
+					Name: "something",
+					Info: &release.Info{
+						Status: &release.Status{
+							Code: release.Status_DEPLOYED,
 						},
 					},
-				}
+				},
+			}
 
-				TestHelm.On("ListReleases", mock.Anything).Return(&rls.ListReleasesResponse{
-					Count:    int64(len(r)),
-					Releases: r,
-				}, errors.New("LsitReleases should fail")).Once()
-				_, err := s.ListReleases()
-				So(err, ShouldNotBeNil)
-			})
-			Convey("Can succeed", func() {
-				r := []*hapi_release5.Release{
-					{
-						Name: "something",
-						Info: &release.Info{
-							Status: &release.Status{
-								Code: release.Status_DEPLOYED,
-							},
+			TestHelm.On("ListReleases", mock.Anything).Return(&rls.ListReleasesResponse{
+				Count:    int64(len(r)),
+				Releases: r,
+			}, errors.New("LsitReleases should fail")).Once()
+			_, err := s.ListReleases()
+			So(err, ShouldNotBeNil)
+		})
+		Convey("Can succeed", func() {
+			r := []*hapi_release5.Release{
+				{
+					Name: "something",
+					Info: &release.Info{
+						Status: &release.Status{
+							Code: release.Status_DEPLOYED,
 						},
 					},
-				}
+				},
+			}
 
-				TestHelm.On("ListReleases", mock.Anything).Return(&rls.ListReleasesResponse{
-					Count:    int64(len(r)),
-					Releases: r,
-				}, nil).Once()
-				releases, err := s.ListReleases()
-				So(err, ShouldBeNil)
-				So(releases, ShouldHaveLength, 1)
-				So(releases[0].Status, ShouldEqual, Status_DEPLOYED)
-				Convey("The value should be greater by one", func() {
+			TestHelm.On("ListReleases", mock.Anything).Return(&rls.ListReleasesResponse{
+				Count:    int64(len(r)),
+				Releases: r,
+			}, nil).Once()
+			releases, err := s.ListReleases()
+			So(err, ShouldBeNil)
+			So(releases, ShouldHaveLength, 1)
+			So(releases[0].Status, ShouldEqual, Status_DEPLOYED)
+			Convey("The value should be greater by one", func() {
 
-				})
 			})
 		})
-
-		Convey("InstallRelease()", func() {
-			Convey("Can fail", func() {
-				TestHelm.On("InstallRelease",
-					mock.AnythingOfType("string"),
-					mock.AnythingOfType("string"),
-					mock.Anything,
-					mock.Anything,
-					mock.Anything,
-					mock.Anything,
-					mock.Anything,
-					mock.Anything,
-				).Return(&rls.InstallReleaseResponse{},
-					errors.New("Sucessfully failed")).Once()
-				_, _, err := s.InstallRelease(&ReleaseMeta{}, []byte{}, true)
-				So(err, ShouldNotBeNil)
-				Print(err)
-			})
-			Convey("Can succeeed", func() {
-				r := &rls.InstallReleaseResponse{
-					Release: &hapi_release5.Release{
-						Name: "something",
-						Info: &release.Info{
-							Status: &release.Status{
-								Code: release.Status_DEPLOYED,
-							},
-						},
-					},
-				}
-				TestHelm.On("InstallRelease",
-					mock.AnythingOfType("string"),
-					mock.AnythingOfType("string"),
-					mock.Anything,
-					mock.Anything,
-					mock.Anything,
-					mock.Anything,
-					mock.Anything,
-					mock.Anything,
-				).Return(r, nil).Once()
-				_, _, err := s.InstallRelease(&ReleaseMeta{}, []byte{}, true)
-				So(err, ShouldBeNil)
-			})
+	})
+}
+func TestInstallRelease(t *testing.T) {
+	s := NewMockSession()
+	Convey("InstallRelease", t, func() {
+		Convey("Can fail", func() {
+			TestHelm.On("InstallRelease",
+				mock.AnythingOfType("string"),
+				mock.AnythingOfType("string"),
+				mock.Anything,
+				mock.Anything,
+				mock.Anything,
+				mock.Anything,
+				mock.Anything,
+				mock.Anything,
+			).Return(&rls.InstallReleaseResponse{},
+				errors.New("Sucessfully failed")).Once()
+			_, _, err := s.InstallRelease(&ReleaseMeta{}, []byte{}, true)
+			So(err, ShouldNotBeNil)
+			Print(err)
 		})
-		Convey("UpgradeRelease()", func() {
-			Convey("Can fail", func() {
-				TestHelm.On("UpdateRelease",
-					mock.AnythingOfType("string"),
-					mock.AnythingOfType("string"),
-					mock.Anything,
-					mock.Anything,
-					mock.Anything,
-				).Return(&rls.UpdateReleaseResponse{},
-					errors.New("Sucessfully failed")).Once()
-				_, err := s.UpgradeRelease(&ReleaseMeta{})
-				So(err, ShouldNotBeNil)
-				Print(err)
-			})
-			Convey("Can succeeed", func() {
-				r := &rls.UpdateReleaseResponse{
-					Release: &hapi_release5.Release{
-						Name: "something",
-						Info: &release.Info{
-							Status: &release.Status{
-								Code: release.Status_DEPLOYED,
-							},
+		Convey("Can succeeed", func() {
+			r := &rls.InstallReleaseResponse{
+				Release: &hapi_release5.Release{
+					Name: "something",
+					Info: &release.Info{
+						Status: &release.Status{
+							Code: release.Status_DEPLOYED,
 						},
 					},
-				}
-				TestHelm.On("UpdateRelease",
-					mock.AnythingOfType("string"),
-					mock.AnythingOfType("string"),
-					mock.Anything,
-					mock.Anything,
-					mock.Anything,
-				).Return(r, nil).Once()
-				_, err := s.UpgradeRelease(&ReleaseMeta{})
-				So(err, ShouldBeNil)
+				},
+			}
+			TestHelm.On("InstallRelease",
+				mock.AnythingOfType("string"),
+				mock.AnythingOfType("string"),
+				mock.Anything,
+				mock.Anything,
+				mock.Anything,
+				mock.Anything,
+				mock.Anything,
+				mock.Anything,
+			).Return(r, nil).Once()
+			_, _, err := s.InstallRelease(&ReleaseMeta{}, []byte{}, true)
+			So(err, ShouldBeNil)
+		})
+	})
+}
+func TestUpgradeRelease(t *testing.T) {
+	s := NewMockSession()
+	Convey("UpgradeRelease", t, func() {
+		Convey("Can fail", func() {
+			TestHelm.On("UpdateRelease",
+				mock.AnythingOfType("string"),
+				mock.AnythingOfType("string"),
+				mock.Anything,
+				mock.Anything,
+				mock.Anything,
+			).Return(&rls.UpdateReleaseResponse{},
+				errors.New("Sucessfully failed")).Once()
+			_, err := s.UpgradeRelease(&ReleaseMeta{})
+			So(err, ShouldNotBeNil)
+			Print(err)
+		})
+		Convey("Can succeeed", func() {
+			r := &rls.UpdateReleaseResponse{
+				Release: &hapi_release5.Release{
+					Name: "something",
+					Info: &release.Info{
+						Status: &release.Status{
+							Code: release.Status_DEPLOYED,
+						},
+					},
+				},
+			}
+			TestHelm.On("UpdateRelease",
+				mock.AnythingOfType("string"),
+				mock.AnythingOfType("string"),
+				mock.Anything,
+				mock.Anything,
+				mock.Anything,
+			).Return(r, nil).Once()
+			_, err := s.UpgradeRelease(&ReleaseMeta{})
+			So(err, ShouldBeNil)
+		})
+	})
+}
+func TestDeleteRelease(t *testing.T) {
+	s := NewMockSession()
+	Convey("DeleteReleases", t, func() {
+		Convey("Can fail", func() {
+			TestHelm.On("DeleteRelease",
+				mock.AnythingOfType("string"),
+				mock.Anything,
+			).Return(&rls.UninstallReleaseResponse{},
+				grpc.Errorf(grpc.Code(grpc.ErrServerStopped), "Failure sucessful")).Once()
+			err := s.DeleteReleases([]*DeleteMeta{
+				{
+					Name:      "this",
+					Namespace: "here",
+				},
 			})
+			So(err, ShouldNotBeNil)
+			Print(err)
+		})
+		Convey("Can succeeed", func() {
+			r := &rls.UninstallReleaseResponse{
+				Release: &hapi_release5.Release{
+					Name: "something",
+					Info: &release.Info{
+						Status: &release.Status{
+							Code: release.Status_DEPLOYED,
+						},
+					},
+				},
+			}
+			TestHelm.On("DeleteRelease",
+				mock.AnythingOfType("string"),
+				mock.Anything,
+			).Return(r, nil).Twice()
+			err := s.DeleteReleases([]*DeleteMeta{
+				{
+					Name:      "this1",
+					Namespace: "here1",
+				},
+				{
+					Name:      "this2",
+					Namespace: "here2",
+				},
+			})
+			So(err, ShouldBeNil)
+		})
+	})
+}
+func TestReleases(t *testing.T) {
+	s := NewMockSession()
+	Convey("Releases", t, func() {
+		Convey("Can fail", func() {
+			r := []*hapi_release5.Release{}
+			TestHelm.On("ListReleases", mock.Anything).Return(&rls.ListReleasesResponse{
+				Count:    int64(len(r)),
+				Releases: r,
+			}, errors.New("successfuly failed")).Once()
+			_, err := s.Releases()
+			So(err, ShouldNotBeNil)
+		})
+		Convey("Can succeed", func() {
+			r := []*hapi_release5.Release{
+				{
+					Name: "something",
+					Info: &release.Info{
+						Status: &release.Status{
+							Code: release.Status_DEPLOYED,
+						},
+					},
+					Chart: &hapi_chart3.Chart{
+						Metadata: &hapi_chart3.Metadata{
+							Name: "this_chartname",
+						},
+					},
+				},
+			}
+			TestHelm.On("ListReleases", mock.Anything).Return(&rls.ListReleasesResponse{
+				Count:    int64(len(r)),
+				Releases: r,
+			}, nil).Once()
+			m, err := s.Releases()
+			So(err, ShouldBeNil)
+			So(m, ShouldContainKey, "this_chartname")
+		})
+	})
+}
+func TestDiffRelease(t *testing.T) {
+	s := NewMockSession()
+	Convey("DiffRelease", t, func() {
+		Convey("Can fail to ReleaseContent", func() {
+			TestHelm.On("ReleaseContent", mock.Anything).Return(&rls.GetReleaseContentResponse{
+				Release: nil,
+			}, errors.New("ReleaeContent should fail")).Once()
+			_, _, err := s.DiffRelease(&ReleaseMeta{
+				Name:      "something",
+				Namespace: "that_namespace",
+			})
+			So(err, ShouldNotBeNil)
 		})
 
-		Convey("DeleteReleases()", func() {
-			Convey("Can fail", func() {
-				TestHelm.On("DeleteRelease",
-					mock.AnythingOfType("string"),
-					mock.Anything,
-				).Return(&rls.UninstallReleaseResponse{},
-					grpc.Errorf(grpc.Code(grpc.ErrServerStopped), "Failure sucessful")).Once()
-				err := s.DeleteReleases([]*DeleteMeta{
-					{
-						Name:      "this",
-						Namespace: "here",
-					},
-				})
-				So(err, ShouldNotBeNil)
-				Print(err)
+		Convey("Can succeed", func() {
+			origRelease, err := yaml.Marshal(metadata{
+				ApiVersion: "v1",
+				Kind:       "Test",
+				Metadata: struct {
+					Namespace string
+					Name      string
+				}{
+					Name:      "testRelease",
+					Namespace: "testNamespace",
+				},
 			})
-			Convey("Can succeeed", func() {
-				r := &rls.UninstallReleaseResponse{
-					Release: &hapi_release5.Release{
-						Name: "something",
-						Info: &release.Info{
-							Status: &release.Status{
-								Code: release.Status_DEPLOYED,
-							},
-						},
-					},
-				}
-				TestHelm.On("DeleteRelease",
-					mock.AnythingOfType("string"),
-					mock.Anything,
-				).Return(r, nil).Twice()
-				err := s.DeleteReleases([]*DeleteMeta{
-					{
-						Name:      "this1",
-						Namespace: "here1",
-					},
-					{
-						Name:      "this2",
-						Namespace: "here2",
-					},
-				})
-				So(err, ShouldBeNil)
-			})
-			Convey("Releases()", func() {
-				Convey("Can fail", func() {
-					r := []*hapi_release5.Release{}
-					TestHelm.On("ListReleases", mock.Anything).Return(&rls.ListReleasesResponse{
-						Count:    int64(len(r)),
-						Releases: r,
-					}, errors.New("successfuly failed")).Once()
-					_, err := s.Releases()
-					So(err, ShouldNotBeNil)
-				})
-				Convey("Can succeed", func() {
-					r := []*hapi_release5.Release{
-						{
-							Name: "something",
-							Info: &release.Info{
-								Status: &release.Status{
-									Code: release.Status_DEPLOYED,
-								},
-							},
-							Chart: &hapi_chart3.Chart{
-								Metadata: &hapi_chart3.Metadata{
-									Name: "this_chartname",
-								},
-							},
-						},
-					}
-					TestHelm.On("ListReleases", mock.Anything).Return(&rls.ListReleasesResponse{
-						Count:    int64(len(r)),
-						Releases: r,
-					}, nil).Once()
-					m, err := s.Releases()
-					So(err, ShouldBeNil)
-					So(m, ShouldContainKey, "this_chartname")
-				})
-			})
-			Convey("DiffRelease()", func() {
-				Convey("Can succeed", func() {
-					origRelease, err := yaml.Marshal(metadata{
-						ApiVersion: "v1",
-						Kind:       "Test",
-						Metadata: struct {
-							Namespace string
-							Name      string
-						}{
-							Name:      "testRelease",
-							Namespace: "testNamespace",
-						},
-					})
-					if err != nil {
-						panic("Could not marshall document")
-					}
+			if err != nil {
+				panic("Could not marshall document")
+			}
 
-					newRelease, err := yaml.Marshal(metadata{
-						ApiVersion: "v1",
-						Kind:       "Test",
-						Metadata: struct {
-							Namespace string
-							Name      string
-						}{
-							Name:      "testRelease",
-							Namespace: "testNamespace2",
-						},
-					})
-					if err != nil {
-						panic("Could not marshall document")
-					}
-					r := &hapi_release5.Release{
-						Name:     "something",
-						Manifest: "\n---\n" + string(origRelease),
-						Info: &release.Info{
-							Status: &release.Status{
-								Code: release.Status_DEPLOYED,
-							},
-						},
-					}
-
-					updateReleaseResp := &rls.UpdateReleaseResponse{
-						Release: &hapi_release5.Release{
-							Name:     "something",
-							Manifest: "\n---\n" + string(newRelease),
-						},
-					}
-
-					TestHelm.On("ReleaseContent", mock.Anything).Return(&rls.GetReleaseContentResponse{
-						Release: r,
-					}, nil).Once()
-					TestHelm.On("UpdateRelease",
-						mock.AnythingOfType("string"),
-						mock.AnythingOfType("string"),
-						mock.Anything,
-						mock.Anything,
-						mock.Anything,
-					).Return(updateReleaseResp, nil).Once()
-					changed, bytes, err := s.DiffRelease(&ReleaseMeta{
-						Name:      "something",
-						Namespace: "that_namespace",
-					})
-					So(err, ShouldBeNil)
-					So(changed, ShouldBeTrue)
-					So(string(bytes), ShouldContainSubstring, "Namespace, testRelease, Test (v1) has been removed")
-				})
+			newRelease, err := yaml.Marshal(metadata{
+				ApiVersion: "v1",
+				Kind:       "Test",
+				Metadata: struct {
+					Namespace string
+					Name      string
+				}{
+					Name:      "testRelease",
+					Namespace: "testNamespace2",
+				},
 			})
+			if err != nil {
+				panic("Could not marshall document")
+			}
+			r := &hapi_release5.Release{
+				Name:     "something",
+				Manifest: "\n---\n" + string(origRelease),
+				Info: &release.Info{
+					Status: &release.Status{
+						Code: release.Status_DEPLOYED,
+					},
+				},
+			}
+
+			updateReleaseResp := &rls.UpdateReleaseResponse{
+				Release: &hapi_release5.Release{
+					Name:     "something",
+					Manifest: "\n---\n" + string(newRelease),
+				},
+			}
+
+			TestHelm.On("ReleaseContent", mock.Anything).Return(&rls.GetReleaseContentResponse{
+				Release: r,
+			}, nil).Once()
+			TestHelm.On("UpdateRelease",
+				mock.AnythingOfType("string"),
+				mock.AnythingOfType("string"),
+				mock.Anything,
+				mock.Anything,
+				mock.Anything,
+			).Return(updateReleaseResp, nil).Once()
+			changed, bytes, err := s.DiffRelease(&ReleaseMeta{
+				Name:      "something",
+				Namespace: "that_namespace",
+			})
+			So(err, ShouldBeNil)
+			So(changed, ShouldBeTrue)
+			So(string(bytes), ShouldContainSubstring, "Namespace, testRelease, Test (v1) has been removed")
 		})
 	})
 }
