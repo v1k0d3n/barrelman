ef64283e5c198a8f674c76eebb1a149e0def2f63
Brandon B. Jozsa <bjozsa@jinkit.com>
2019-01-13 10:54:04 -0700

    Add version information to Barrelman
    This is just _a_ method for adding version information to Barrelman,
    while it may not exactly be _the best_ method, which would require a
    little more time/thought. This would require us to create a Makefile
    so that we can pass docker build args according to tag/commit.
    
    In this form, we would be required to do the following for docker
    builds:
    
    ```
    export VERSION="v0.2.3"
    export COMMIT="$(git rev-parse --short HEAD)"
    
    docker build --build-arg COMMIT="$(git rev-parse --short HEAD)" --build-arg VERSION="v0.2.4" .
    ```
    
    I think this is fine for now, since we should have a Makefile soon.
    I won't be offended if this gets rejected for something better though :)
    
    -v1k
    
    Signed-off-by: Brandon B. Jozsa <bjozsa@jinkit.com>

--- barrelman.go
+++ barrelman.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/charter-se/barrelman/cmd"
 )
 
+var (
+	version = "No version declared at build"
+	commit  = "No git commit declared at build"
+)
+
 func main() {
+	fmt.Printf("Version: %s\n", version)
+	fmt.Printf("Commit: %s\n", commit)
+
 	cmd.Execute()
 }
