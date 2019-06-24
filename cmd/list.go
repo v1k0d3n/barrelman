7f0565a1b4cd1be6cf0b01b5e8a4d93d31813bca
Dan Taylor <C-Dan.Taylor@charter.com>
2019-06-24 12:07:00 -0600

    description updates

--- cmd/list.go
+++ cmd/list.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/lithammer/dedent"
 	"github.com/spf13/cobra"
 
 	"github.com/charter-oss/barrelman/pkg/barrelman"
@@ -9,10 +10,24 @@ import (
 )
 
 func newListCmd(cmd *barrelman.ListCmd) *cobra.Command {
+
+	longDesc := dedent.Dedent(`
+		barrelman list 
+			Display Barrelman manifests stored in the kuerbenetes cluster.
+			Manifest names can be used with the rollback command. 
+		
+		barrelman list [manifest name]
+			Display releases and versions associated with the specified manifest.
+	`)
+
+	shortDesc := dedent.Dedent(`
+		list Barrelman manifests and releases
+	`)
+
 	cobraCmd := &cobra.Command{
 		Use:   "list [manifest name]",
-		Short: "list something",
-		Long:  `lists the current releases associated with the provided manifest name`,
+		Short: shortDesc,
+		Long:  longDesc,
 		RunE: func(cobraCmd *cobra.Command, args []string) error {
 			if len(args) > 0 {
 				cmd.ManifestName = args[0]
