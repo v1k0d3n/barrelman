9dccf0219a031ef5c3c91d9dd654e26fcc2d44d9
Ubuntu <ubuntu@flagship.sh>
2019-08-02 03:06:11 +0000

    changing delete test

--- e2e/delete_test.go
+++ e2e/delete_test.go
@@ -10,27 +10,33 @@ import (
 )
 
 func TestAccBarrelmanDeleteCommand(t *testing.T) {
-	podNS := "example-go-web-service-for-delete"
-	barrelmanPath, _ := os.Getwd()
-	expectedPodCountForDelete := 0
-	retryCount, _ := strconv.Atoi(os.Getenv("RETRYCOUNTACC"))
-	BM_TEST_E2E, _ := strconv.ParseBool(os.Getenv("BM_TEST_E2E"))
-	if BM_TEST_E2E==true {
-		Convey("Given a manifest", t, func() {
-			out, err := exec.Command(barrelmanPath+"/../barrelman", "apply", "testdata/manifestForDeleteOps.yaml").CombinedOutput()
-			So(err, ShouldBeNil)
-			So(string(out), ShouldContainSubstring, "Barrelman")
-			Convey("When delete is run", func() {
-				out, err := exec.Command(barrelmanPath+"/../barrelman", "delete", "testdata/manifestForDeleteOps.yaml").CombinedOutput()
-				So(err, ShouldBeNil)
-				So(string(out), ShouldContainSubstring, "deleting release")
-
-				Convey("The pod count should be 0", func() {
-					So(retryUntilExpectedPodCount(retryCount, podNS, expectedPodCountForDelete), ShouldBeNil)
-				})
-			})
-		})
-	} else {
+	if accUserValue := os.Getenv("BM_TEST_E2E"); accUserValue == "" {
+		t.Log("To run Acceptance tests, run 'make testacc")
 		t.Skip("Skipping Delete Test")
 	}
+	bmBin := os.Getenv("BM_BIN")
+	if bmBin == "" {
+		t.Fatal("Barrelman binary environment variable BM_BIN not set")
+	}
+	podNS := "example-go-web-service-for-delete"
+	expectedPodCountForDelete := 0
+	retryCount, _ := strconv.Atoi(os.Getenv("RETRYCOUNTACC"))
+	interval, _ := strconv.Atoi(os.Getenv("INTERVALTIME"))
+	Convey("Given a manifest", t, func() {
+		out, err := exec.Command(bmBin, "apply", "testdata/manifestForDeleteOps.yaml").CombinedOutput()
+		So(err, ShouldBeNil)
+		So(string(out), ShouldContainSubstring, "Barrelman")
+		Convey("When delete is run", func() {
+			out, err := exec.Command(bmBin, "delete", "testdata/manifestForDeleteOps.yaml").CombinedOutput()
+			So(err, ShouldBeNil)
+			So(string(out), ShouldContainSubstring, "deleting release")
+
+			Convey("The pod count should be 0", func() {
+				f := func() error {
+					return checkPodCount(podNS, podName, expectedPodCountForDelete)
+				}
+				So(retry(f, retryCount, interval, []string{"WrongNumberOfPods"}), ShouldBeNil)
+			})
+		})
+	})
 }
